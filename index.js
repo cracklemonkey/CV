930b007691f75db476569d09bc6e95f71fe14d97
daniel stupar <danielstupar95@gmail.com>
2021-06-02 14:29:49 +0200

    la til radio buttons slik man kan endre font,størelse på teksten på personalia og at man kan endre backrund på sectionen

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
 
 
-
+//Buttons
 const keyBtn = document.getElementById("keyBtn");
 const eduBtn = document.getElementById("eduBtn");
 const workBtn = document.getElementById("workBtn");
+//paragraphs
 const keyPara = document.getElementById('keyPara');
 const eduPara = document.getElementById('eduPara');
 const workPara = document.getElementById('workPara');
+//stars
 const allStar = document.getElementById('allStar');
 const fiveStar = document.getElementById('fiveStar');
 const fourStar = document.getElementById('fourStar');
 const threeStar = document.getElementById('threeStar');
 const twoStar = document.getElementById('twoStar');
 const oneStar = document.getElementById('oneStar');
+/*//radio buttons textsize
+const threeStar = document.getElementById('threeStar');
+const twoStar = document.getElementById('twoStar');
+const oneStar = document.getElementById('oneStar');
+//radio buttons font family
+const threeStar = document.getElementById('threeStar');
+const twoStar = document.getElementById('twoStar');
+const oneStar = document.getElementById('oneStar');
+//radio buttons header backround
+const threeStar = document.getElementById('threeStar');
+const twoStar = document.getElementById('twoStar');
+const oneStar = document.getElementById('oneStar');
 
-
+*/
 
 function sortStarList(starId){
 
     let lis = document.getElementById('dataList').getElementsByTagName('li');
 
     for (let i = 0; i < lis.length; i++) {
-    const x = lis[i];
+    const listElement = lis[i];
 
     if(starId == null){
-        x.classList.remove('hidden')
+        listElement.classList.remove('hidden')
     }
     
     else{
-    if(x.classList.contains(starId)){
-        x.classList.remove('hidden')
+    if(listElement.classList.contains(starId)){
+        listElement.classList.remove('hidden')
     }else{
-        x.classList.add('hidden')
+        listElement.classList.add('hidden')
     }
 }
    
@@ -41,6 +55,60 @@ function sortStarList(starId){
     
 
 }
+
+
+function radioHandler(event) {
+    
+if(event.target.name === "textSize"){
+    
+    if(event.target.id === "textSizesmall"){
+        const personalia = document.getElementById('personalia')
+        personalia.style.fontSize = "10px"
+    }else if(event.target.id === "textSizesmedium"){
+        const personalia = document.getElementById('personalia')
+        personalia.style.fontSize = "20px"
+    }else{
+        const personalia = document.getElementById('personalia')
+        personalia.style.fontSize = "40px"
+    }}
+else if(event.target.name === "fontFamily"){
+        console.log("inni else iffen")
+        if(event.target.id === "newRoman"){
+            console.log("roman")
+            const personalia = document.getElementById('personalia')
+            personalia.style.fontFamily = "Times, serif"
+        }else if(event.target.id === "sSerif"){
+            const personalia = document.getElementById('personalia')
+            personalia.style.fontFamily = "sans-serif"
+        }else{
+            const personalia = document.getElementById('personalia')
+            personalia.style.fontFamily = "fantasy"
+}}else{
+        
+    if(event.target.id === "backYellow"){
+            const header = document.getElementById('header')
+            header.style.background = "yellow"
+        }else if(event.target.id === "backlightskyblue"){
+            const header = document.getElementById('header')
+            header.style.background = "lightskyblue"
+        }else{
+            const header = document.getElementById('header')
+            header.style.background = "red"
+
+        }
+    }
+}
+document.querySelectorAll("input[name='textSize']").forEach((input) => {
+    input.addEventListener('change', radioHandler);
+});
+
+document.querySelectorAll("input[name='fontFamily']").forEach((input) => {
+    input.addEventListener('change', radioHandler);
+});
+
+document.querySelectorAll("input[name='HeaderBackround']").forEach((input) => {
+    input.addEventListener('change', radioHandler);
+});
 
 keyBtn.addEventListener('click', () => {
     keyPara.classList.toggle('hidden');    
@@ -79,3 +147,4 @@ twoStar.addEventListener('click', () => {
 oneStar.addEventListener('click', () => {
     sortStarList('1star');    
 })
+
